c8fb9be8c76183848298659437230a2c6674ab6d
Kirill <They@example.com>
2024-11-07 20:41:24 +0300

    сделан 7 пример

---
+++ primer7/script.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+let number1 = Number(prompt('Введите первое число:'));
+let number2 =  Number(prompt('Введите второе число:'));
+let number2 =  Number(prompt('Введите третье число:'));
+let delenie = (number1 + number2 + number3) / 3;
+console.log("среднее арифметическое: " + srednee);
