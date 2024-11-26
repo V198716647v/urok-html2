82b692b92c15c3963f2a9773ef947be2bf4b6f25
Kirill <They@example.com>
2024-11-26 20:39:43 +0300

    Сделано dz6/task1

--- primer7/script.js
+++ primer7/script.js
@@ -1,6 +1,6 @@
 const prompt = require('prompt-sync')();
 let number1 = Number(prompt('Введите первое число:'));
 let number2 =  Number(prompt('Введите второе число:'));
-let number2 =  Number(prompt('Введите третье число:'));
-let delenie = (number1 + number2 + number3) / 3;
+let number3 =  Number(prompt('Введите третье число:'));
+let srednee = (number1 + number2 + number3) / 3;
 console.log("среднее арифметическое: " + srednee);
